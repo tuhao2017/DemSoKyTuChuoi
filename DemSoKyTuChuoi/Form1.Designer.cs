8c499cf902c99a2bbefb74430fcbe4efdb60fb46
NATIVE <NATIVE@DESKTOP-UK01LRC>
2018-01-28 23:34:40 +0700

    Add project files.

---
+++ DemSoKyTuChuoi/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DemSoKyTuChuoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChuoi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ketQua";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập chuỗi";
+            // 
+            // txtChuoi
+            // 
+            this.txtChuoi.Location = new System.Drawing.Point(97, 55);
+            this.txtChuoi.Name = "txtChuoi";
+            this.txtChuoi.Size = new System.Drawing.Size(141, 20);
+            this.txtChuoi.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kết Quả";
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(97, 81);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(141, 20);
+            this.txtKetQua.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 166);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtChuoi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChuoi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKetQua;
+    }
+}
+
---
+++ DemSoKyTuChuoi/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemSoKyTuChuoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * struc chuoi
+         */
+        
+        struct String
+        {
+            public string Chuoi;
+        }
+
+        /*
+         * Hàm đếm chuỗi
+         */
+
+        private int DemKyTuTrongChuoi(char[] bienChuoi)
+        {
+            int dem = 0;
+
+            int i = 0;
+
+            while (bienChuoi[i] != ' ')
+            {
+                dem++;
+                i++;
+            }
+                
+            return dem; 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mangChuoi = txtChuoi.Text;
+
+            int ketqua = 0; 
+
+            char[] a = mangChuoi.ToCharArray();
+
+            ketqua = DemKyTuTrongChuoi(a);
+
+            txtKetQua.Text = ketqua.ToString();
+        }
+    }
+}
